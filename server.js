69ccc702b6c7b999c0950e49d54b0cbf6024a088
Glitch (hello-express) <none>
2020-12-20 19:10:43 +0000

    Moves json into its own file maritimeLocations.json deletes the now superflous tracery.js

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
 const express = require("express");
 const tracery = require('tracery-grammar');
+const maritimeLocationsJson = require('./maritimeLocations.json');
 const app = express();
 
-let json = {
-    "line":[
-        "#descriptiveAdj# #establishedLoc#",
-        "#descriptiveAdj# #possessiveAdj# #establishedLoc#",
-        "#descriptiveAdj# #geographicalLoc#",
-        "#descriptiveAdj# #possessiveAdj# #geographicalLoc#",
-        "#descriptiveAdj# #establishedLoc# #postnominalModifers#",
-        "#possessiveAdj# #geographicalLoc# #postnominalModifers#",
-        "#descriptiveAdj# #geographicalLoc# #postnominalModifers#",
-        "#possessiveAdj# #geographicalLoc# #postnominalModifers#",
-        "#establishedLoc# #postnominalModifers#",
-        "#geographicalLoc# #postnominalModifers#"
-  
-    ],
-    "descriptiveAdj":["Blackend","Broken","Concealed","Dreaded","Fancy","Grand","Hidden","Mystic", "Plentiful", "Ravaged", "Royal", "Salted", "Scorched", "Secluded", "Secret", "Splendid", "Stolen", "Sunken", "Sweet", "Wrecked"],
-    "possessiveAdj":["Barracuda's", "Captain's", "Dragon's", "Guild's", "Hunter's", "Kraken's", "Maiden's", "Mermaid's", "Order's", "Parrot's", "Raider's", "Sailor's", "Shark's", "Shipwreck's", "Siren's", "Storm's", "Thieves'", "Triton's", "Turtle's", "Wanderer's"],
-    "establishedLoc":["Asyulum", "Bounty", "Den", "Fort", "Gem", "Harbor", "Haven", "Hideout", "Hold", "Jewel", "Keep", "Port", "Refuge", "Rest", "Retreat", "Sanctuary", "Shelter", "Stronghold", "Treasure", "Trove"],
-    "geographicalLoc": ["Archipelago", "Atoll", "Bay", "Bluffs", "Cliffs", "Cove", "Crag", "Enclave", "Groves", "Hollow", "Island", "Isle", "Lagoon", "Peninsula", "Reef", "Ridge", "Rock", "Sands", "Shallows", "Shores"],
-    "postnominalModifers": ["amidst the Maelstroms", "between the Waves", "in the Mists", "in the Shadows", "of a Thousand Coins", "of Plentiful Riches", "of Skulls", "of the Countless Wrecks", "of the Cursed", "of the Damned", "of the Depths", "of the Homesick", "of the Lawless", "of the Lost Souls", "of the Moon", "of the Raven", "of the Sunset", "of the Sweet Embrace", "of the Tides", "of Whispers"]
-};
-
-var grammar = tracery.createGrammar(json);
+var grammar = tracery.createGrammar(maritimeLocationsJson);
 for (let i = 0; i < 3; i++) {
   var print = grammar.flatten("#line#");
   console.log(print);
