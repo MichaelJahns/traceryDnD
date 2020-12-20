f02f0c15d5f6c34cc3bf9333eb50d5d907aee35c
Glitch (hello-express) <none>
2020-12-20 21:03:06 +0000

    👜🏯 Checkpoint ./server.js:10294375/5224

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
 const tracery = require("tracery-grammar");
 
-var PROTO_PATH = './helloworld.proto';
+var PROTO_PATH = "./helloworld.proto";
 
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function sayHello(call, callback) {
-  callback(null, {message: 'Hello ' + call.request.name});
+  callback(null, { message: "Hello " + call.request.name });
 }
-console.log("echo")
-function sayHelloAgain(call, callback){
-  callback(null, {message: 'Hello Once Again, ' + call.request.name})
+
+function sayHelloAgain(call, callback) {
+  callback(null, { message: "Hello Once Again, " + call.request.name });
 }
 
 const maritimeLocationsJson = require("./maritimeLocations.json");
@@ -31,19 +30,23 @@ function getPirateLocations(number) {
     let print = grammar.flatten("#line#");
     locationsArray.push(print);
   }
-  console.log(locationsArray)
+  console.log(locationsArray);
 }
 
 function main() {
   var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, 
-    {
-      sayHello: sayHello,
-      sayHelloAgain: sayHelloAgain
-    });
-    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
+  server.addService(hello_proto.Greeter.service, {
+    sayHello: sayHello,
+    sayHelloAgain: sayHelloAgain
   });
+  server.bindAsync(
+    "0.0.0.0:50051",
+    grpc.ServerCredentials.createInsecure(),
+    () => {
+      server.start();
+    }
+  );
 }
 
 main();
+console.log("echo");
